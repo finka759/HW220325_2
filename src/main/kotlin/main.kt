90befe37431c785aa47bdcb9a4d7fb751df31b97
finka759 <finka759@gmail.com>
2025-03-25 21:41:16 +0300

    mastercard edit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,11 +4,11 @@ fun main() {
     val curent_amount: Int = 76000
     val prev_amounts_in_day: Int = 150_000
     val prev_amounts_in_mounth: Int = 600_000
-    val cards_tipe: String = "Mastercard" //"Visa" "Мир"
+    val cards_type: String = "Mastercard" //"Visa" "Мир"
 
 
     if ((curent_amount + prev_amounts_in_day) <= limit_day && (curent_amount + prev_amounts_in_mounth) <= limit_month) {
-        val realTax = getTax(cards_tipe, prev_amounts_in_mounth, curent_amount)
+        val realTax = getTax(cards_type, prev_amounts_in_mounth, curent_amount)
         if (realTax >= 0) {
             println("Комиссия составит: $realTax")
         } else {
@@ -22,9 +22,10 @@ fun main() {
 }
 
 fun get_tax_mastercard(prev_amounts_in_mounth: Int = 0, curent_amount: Int): Int {
-    return if (curent_amount <= 75000) {
+    val exemption = 75_000
+    return if (curent_amount + prev_amounts_in_mounth <= exemption) {
         0
-    } else ((curent_amount + prev_amounts_in_mounth - 75000) * 0.006 + 20).toInt()
+    } else ((curent_amount + prev_amounts_in_mounth - exemption) * 0.006 + 20).toInt()
 }
 
 fun get_tax_visa(curent_amount: Int): Int {
@@ -42,10 +43,10 @@ fun get_tax_mir(): Int {
 }
 
 fun getTax(
-    cards_tipe: String = "Мир",
+    cards_type: String = "Мир",
     prev_amounts_in_mounth: Int = 0,
     curent_amount: Int
-) = when (cards_tipe) {
+) = when (cards_type) {
     "Mastercard" -> get_tax_mastercard(prev_amounts_in_mounth, curent_amount)
     "Visa" -> get_tax_visa(curent_amount)
     "Мир" -> get_tax_mir()
